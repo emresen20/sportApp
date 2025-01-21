0560607441738b87135424f28790e382b655f3b3
emresen20 <emrebernarn@gmail.com>
2025-01-21 19:00:25 +0300

    Initial commit

---
+++ navigation/StackNavigator.js
@@ -0,0 +1,102 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import BookScreen from '../screens/BookScreen';
+import PlayScreen from '../screens/PlayScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const StackNavigator = () => {
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+
+  function BottomTabs() {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: 'Home', // Tab etiketi
+            tabBarIcon: ({focused}) => (
+              <Icon
+                name={focused ? 'home' : 'home-outline'}
+                size={24}
+                color={focused ? 'green' : 'gray'} // Iconun rengi odaklanmışken ve odaklanmamışken
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Play"
+          component={PlayScreen}
+          options={{
+            tabBarLabel: 'Play', // Tab etiketi
+            tabBarIcon: ({focused}) => (
+              <Icon
+                name={focused ? 'play' : 'play-outline'}
+                size={24}
+                color={focused ? 'green' : 'gray'} // Iconun rengi odaklanmışken ve odaklanmamışken
+              />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Book"
+          component={BookScreen}
+          options={{
+            tabBarLabel: 'Book', // Tab etiketi
+            tabBarIcon: ({focused}) => (
+              <Icon
+                name={focused ? 'book' : 'book-outline'}
+                size={24}
+                color={focused ? 'green' : 'gray'} // Iconun rengi odaklanmışken ve odaklanmamışken
+              />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: 'Profile', // Tab etiketi
+            tabBarIcon: ({focused}) => (
+              <Icon
+                name={focused ? 'person' : 'person-outline'}
+                size={24}
+                color={focused ? 'green' : 'gray'} // Iconun rengi odaklanmışken ve odaklanmamışken
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
+
+  function MainStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={BottomTabs}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      <MainStack />
+    </NavigationContainer>
+  );
+};
+
+export default StackNavigator;
+
+const styles = StyleSheet.create({});

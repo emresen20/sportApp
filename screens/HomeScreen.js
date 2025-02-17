5254d5e21f1f07a581459d5e94a1d32db650bfc0
emresen20 <emrebernarn@gmail.com>
2025-02-17 20:33:16 +0300

    Second Part of HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -68,11 +68,61 @@ const HomeScreen = () => {
         </View>
       </View>
 
-      <View>
-        <View>
-          <Text> Gear Up Your Game</Text>
+      <View style={styles.getView}>
+        <View style={styles.gearView}>
+          <Text style={styles.gearText}> Gear Up For Your Game</Text>
         </View>
+
+        <View style={styles.badmintonView}>
+          <Text style={{fontSize: hp('1.9%')}}>Badminton Activity</Text>
+
+          <Pressable style={styles.pressableview}>
+            <Text style={{textAlign: 'center'}}>View</Text>
+          </Pressable>
+        </View>
+
+        <Text style={styles.nogameText}>You have no Games Today</Text>
+
+        <Pressable style={styles.viewCalendar}>
+            <Text style={styles.TextCalendar}>
+                View My Calendar
+            </Text>
+        </Pressable>
       </View>
+
+      <View style={styles.footballcontaienerview}>
+        <Pressable style={{flex:1}}>
+            <View style={styles.footbalimageview}>
+                <Image 
+                source={{uri:'https://images.pexels.com/photos/262524/pexels-photo-262524.jpeg?auto=compress&cs=tinysrgb&w=800'}}
+                style={styles.footballimage}
+                 />
+            </View>
+
+            <Pressable style={styles.playpresabble}>
+                <View>
+                    <Text style={styles.firsttextonpicture}>Play</Text>
+                    <Text style={styles.secondttextonpicture}>Find Players and join their activities</Text>
+                </View>
+            </Pressable>
+        </Pressable>
+        <Pressable style={{flex:1}}>
+            <View style={styles.footbalimageview}>
+                <Image 
+                source={{uri:'https://images.pexels.com/photos/3660204/pexels-photo-3660204.jpeg?auto=compress&cs=tinysrgb&w=800'}}
+                style={styles.footballimage}
+                 />
+            </View>
+
+            <Pressable style={styles.playpresabble}>
+                <View>
+                    <Text style={styles.firsttextonpicture}>Book</Text>
+                    <Text style={styles.secondttextonpicture}>Book your slots in venues nearby</Text>
+                </View>
+            </Pressable>
+        </Pressable>
+      </View>
+
     </ScrollView>
   );
 };
@@ -97,11 +147,15 @@ const styles = StyleSheet.create({
   fitview: {
     padding: hp('1.8%'),
     backgroundColor: 'white',
-    margin: hp('1.8%'),
+    margin: hp('1.6%'),
     borderRadius: hp('1.8%'),
     flexDirection: 'row',
     alignItems: 'center',
     gap: hp('2%'),
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.25,
+    shadowRadius: 2,
   },
   leftheader: {
     flexDirection: 'row',
@@ -123,4 +177,82 @@ const styles = StyleSheet.create({
     marginTop: hp('0.5%'),
     color: 'gray',
   },
+  gearText: {
+    color: '#484848',
+    fontSize: hp('1.8%'),
+  },
+  getView: {
+    backgroundColor: 'white',
+    padding: hp('1.8%'),
+    margin: hp('1.6%'),
+    borderRadius: hp('2%'),
+  },
+  gearView: {
+    paddingHorizontal: hp('1.3%'),
+    backgroundColor: '#E0E0E0',
+    paddingVertical: hp('0.3%'),
+    borderRadius: 4,
+    width: wp('50%'),
+    marginVertical: 5,
+  },
+  badmintonView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  pressableview: {
+    padding: '2.4%',
+    backgroundColor: 'white',
+    borderRadius: 7,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    width: wp('18%'),
+  },
+  nogameText:{
+    marginTop:hp('0.3%'),
+    color:'gray'
+  },
+  viewCalendar:{
+    marginTop:hp('1.5'),
+    marginLeft:'auto',
+    marginRight:'auto',
+    marginBottom:hp('0.4%')
+  },
+  TextCalendar:{
+    fontSize:hp('1.9'),
+    fontWeight:"600",
+    textDecorationLine:'underline'
+  },
+  footballimage:{
+    width:wp('43'),
+    height:hp('16'),
+    borderRadius:10
+  },
+  playpresabble:{
+    backgroundColor:'white',
+    padding:('6'),
+    width:wp('43'),
+    borderRadius:10
+
+  },
+  footbalimageview:{
+    borderRadius:10
+  },
+  footballcontaienerview:{
+    padding:hp('1.7'),
+    flexDirection:'row',
+    alignItems:'center',
+    gap:hp('1.6')
+  },
+  firsttextonpicture:{
+    fontSize:hp('1.7'),
+    fontWeight:'500'
+  },
+  secondttextonpicture:{
+    fontSize:hp('1.7'),
+    color:'gray',
+    marginTop:hp('0.5')
+  }
 });

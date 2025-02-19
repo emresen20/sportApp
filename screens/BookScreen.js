46628dcc9fe529d40fea53ee4f30890fd339943c
emresen20 <emrebernarn@gmail.com>
2025-02-20 00:16:59 +0300

    tabbar for bookscreen

--- screens/BookScreen.js
+++ screens/BookScreen.js
@@ -1,11 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View,  Image, } from 'react-native'
 import React from 'react'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const BookScreen = () => {
   return (
-    <View>
-      <Text>BookScreen</Text>
-    </View>
+    <SafeAreaView style={{flex:1,backgroundColor:'#f5f5f5'}}>
+        <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          padding: 12,
+        }}>
+        <View style={{flexDirection: 'row', alignItems: 'center', gap: 5}}>
+          <Text style={{fontSize: 16, fontWeight: '500'}}>Emre Shen</Text>
+          <MaterialIcons name="keyboard-arrow-down" size={24} color="black" />
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: 10,
+          }}>
+          <Ionicons name="chatbox-outline" size={24} color="black" />
+          <Ionicons name="notifications-outline" size={24} color="black" />
+
+          <View>
+            <Image
+              style={{width: 30, height: 30, borderRadius: 15}}
+              source={{
+                uri: 'https://lh3.googleusercontent.com/a/ACg8ocJJo_bZya4l3sXsEq0-34iEFzIKuSthszhBrNKVfZteK7nI84BW=s576-c-no',
+              }}
+            />
+          </View>
+        </View>
+      </View>
+
+    </SafeAreaView>
   )
 }
 

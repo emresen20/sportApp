583ab54d3e101b472d6c13852bb403a46cf494d0
emresen20 <emrebernarn@gmail.com>
2025-02-20 14:31:07 +0300

    BookScreen design and flatlist

--- screens/BookScreen.js
+++ screens/BookScreen.js
@@ -6,12 +6,101 @@ import {
   Image,
   TextInput,
   Pressable,
+  FlatList,
 } from 'react-native';
 import React from 'react';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import VenueCard from './components/VenueCard';
 
 const BookScreen = () => {
+  const venues = [
+    {
+      name: '147 One Four Seven Snooker, Billiards and Pool Sports Academy',
+      rating: 4,
+      deferLink: 'https://playo.page.link/ry8TT',
+      fullLink:
+        'https://playo.co/venue/?venueId=4ec5b58f-d58f-4ce1-8c84-2caa63007ecc',
+      avgRating: 4,
+      image:'https://playo.gumlet.io/147ONEFOURSEVENSNOOKERBILLIARDSANDPOOLSPORTSACADEMY/1.png?w=700&format=webp&q=30&overlay=https://playo-website.gumlet.io/playo-website-v2/logos-icons/playo-logo.png&overlay_width_pct=0.2&overlay_height_pct=1&overlay_position=bottomright',
+      ratingCount: 3,
+      lat: 12.9341796,
+      adress:'5, Bhuvanappa Layout, Kaveri Layout, Suddagunte Palya, Bengaluru, Karnataka 560029',
+      lng: 77.6101537,
+      icon: 'https://maps.google.com/mapfiles/kml/paddle/4-lv.png',
+      filter_by: ['Pool', 'Snooker'],
+      info: '\n    <h3>147 One Four Seven Snooker, Billiards and Pool Sports Academy</h3>\n    <strong>Ratings:</strong> 4 [3]<br/>\n    <strong>Sports:</strong> Pool, Snooker<br/>\n    <strong>Phone:</strong> 81059 00199<br/>\n    <a href="https://playo.page.link/ry8TT" target="_blank">https://playo.page.link/ry8TT</a><br/>\n    ',
+    },
+    {
+      name: '33intact Clinics - Indiranagar',
+      rating: 5,
+      deferLink: 'https://z34v4.app.goo.gl/gNe3',
+      fullLink:
+        'https://playo.co/venue/?venueId=81bd286d-77b5-4fc2-adf0-26bd159ab965',
+      lat: 12.97738637037637,
+      lng: 77.64410844836385,
+      avgRating: 5,
+      image:'https://playo.gumlet.io/AADUKALAMMULTISPORTSFACILITYANDTRAININGACADEMY/33intactClinicsIndiranagar1663162050263.jpeg?w=700&format=webp&q=30&overlay=https://playo-website.gumlet.io/playo-website-v2/logos-icons/playo-logo.png&overlay_width_pct=0.2&overlay_height_pct=1&overlay_position=bottomright',
+      ratingCount: 7,
+      adress:'277, Ground Floor, 1st Main Rd, HAL 2nd Stage, Indiranagar, Bengaluru, Karnataka - 560038',
+      icon: 'https://maps.google.com/mapfiles/kml/paddle/5-lv.png',
+      filter_by: [
+        'Consultancy',
+        'Physiotherapy',
+        'Reflexology',
+        'Sports Massage',
+      ],
+      info: '\n    <h3>33intact Clinics - Indiranagar</h3>\n    <strong>Ratings:</strong> 5 [7]<br/>\n    <strong>Sports:</strong> Consultancy, Physiotherapy, Reflexology, Sports Massage<br/>\n    <strong>Phone:</strong> 9206660840<br/>\n    <a href="https://z34v4.app.goo.gl/gNe3" target="_blank">https://z34v4.app.goo.gl/gNe3</a><br/>\n    ',
+    },
+    {
+      avgRating: 3.55,
+      name: '4S Shuttle Smash Sports Studio - Managed by Flick Sport Center',
+      ratingCount: 337,
+      deferLink: 'https://z34v4.app.goo.gl/q1nf',
+      fullLink:
+        'https://playo.co/venue/?venueId=cdbc6917-01b2-4ee1-81ef-cbeeda5b6eeb',
+      lat: 12.901148,
+      lng: 77.597311,
+      adress:'New No: 165/14, Doresanipalya, Landmark Behind Kalyani Magnum Software Park, Rose Garden Rd, Krishnaraju Layout, Amalodbhavi Nagar, Pand, Bengaluru, Karnataka 560076',
+      image:'https://playo.gumlet.io/4SSHUTTLESMASHSPORTSSTUDIO/4sshuttlesmashsportsstudio5.jpg?w=700&format=webp&q=30&overlay=https://playo-website.gumlet.io/playo-website-v2/logos-icons/playo-logo.png&overlay_width_pct=0.2&overlay_height_pct=1&overlay_position=bottomright',
+      rating: 3,
+      icon: 'https://maps.google.com/mapfiles/kml/paddle/3-lv.png',
+      filter_by: ['Badminton'],
+      info: '\n    <h3>4S Shuttle Smash Sports Studio - Managed by Flick Sport Center</h3>\n    <strong>Ratings:</strong> 3.55 [337]<br/>\n    <strong>Sports:</strong> Badminton<br/>\n    <strong>Phone:</strong> 9743504189<br/>\n    <a href="https://z34v4.app.goo.gl/q1nf" target="_blank">https://z34v4.app.goo.gl/q1nf</a><br/>\n    ',
+    },
+    {
+      avgRating: 4.89,
+      name: '7 Eleven Badminton Academy',
+      ratingCount: 9,
+      fullLink:
+        'https://playo.co/venue?venueId=06d5685e-0abf-4a84-a8cc-bf7fa940ae25',
+      deferLink: 'https://z34v4.app.goo.gl/X5tg',
+      lat: 12.7016928,
+      lng: 77.8472922,
+      adress:'524/2,525/1b, Near Hp Petrol Bunk, Royakottai Road, Karapalli, Hosur-635109',
+      image:'https://playo.gumlet.io/7ELEVENBADMINTONACADEMY20230112124743905590/7ElevenBadmintonAcademy1673527526517.jpg?w=700&format=webp&q=30&overlay=https://playo-website.gumlet.io/playo-website-v2/logos-icons/playo-logo.png&overlay_width_pct=0.2&overlay_height_pct=1&overlay_position=bottomright',
+      rating: 4,
+      icon: 'https://maps.google.com/mapfiles/kml/paddle/4-lv.png',
+      filter_by: ['Badminton'],
+      info: '\n    <h3>7 Eleven Badminton Academy</h3>\n    <strong>Ratings:</strong> 4.89 [9]<br/>\n    <strong>Sports:</strong> Badminton<br/>\n    <strong>Phone:</strong> 9894771017<br/>\n    <a href="https://z34v4.app.goo.gl/X5tg" target="_blank">https://z34v4.app.goo.gl/X5tg</a><br/>\n    ',
+    },
+    {
+      ratingCount: 82,
+      avgRating: 4.05,
+      name: '91 Sporting Co. Badminton Arena - Scholars Sports Arena',
+      fullLink:
+        'https://playo.co/venue?venueId=f596daa9-5b6c-46c4-847c-b64767d5b3fc',
+      deferLink: 'https://z34v4.app.goo.gl/5zoj',
+      adress:'Google Plus Code: 2JQ3+7R; Address: #1, Sy No 28/3, (RKB Estate), V. Nagenahalli Main Road (Near Kanaka Nagar), R.T. Nagar Post, Bangalore',
+      lat: 13.038189,
+      lng: 77.604562,
+      image:'https://playo.gumlet.io/VKMATCHPOINTRTNAGAR20200201082734053724/91SportingCoBadmintonArenaScholarsSportsArena1692089657817.jpg?w=700&format=webp&q=30&overlay=https://playo-website.gumlet.io/playo-website-v2/logos-icons/playo-logo.png&overlay_width_pct=0.2&overlay_height_pct=1&overlay_position=bottomright',
+      rating: 4,
+      icon: 'https://maps.google.com/mapfiles/kml/paddle/4-lv.png',
+      filter_by: ['Badminton', 'Table Tennis'],
+      info: '\n    <h3>91 Sporting Co. Badminton Arena - Scholars Sports Arena</h3>\n    <strong>Ratings:</strong> 4.05 [82]<br/>\n    <strong>Sports:</strong> Badminton, Table Tennis<br/>\n    <strong>Phone:</strong> 9611667891<br/>\n    <a href="https://z34v4.app.goo.gl/5zoj" target="_blank">https://z34v4.app.goo.gl/5zoj</a><br/>\n    ',
+    },
+  ];
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#f5f5f5'}}>
       <View
@@ -80,6 +169,13 @@ const BookScreen = () => {
           </View>
         ))}
       </Pressable>
+      <FlatList
+        data={venues}
+        renderItem={({item}) => <VenueCard item={item} 
+        contentContainerStyle={{paddingBottom:20}}
+        showVerticalScrollIndicator={false}
+        />}
+      />
     </SafeAreaView>
   );
 };
---
+++ screens/components/VenueCard.js
@@ -0,0 +1,69 @@
+import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+const VenueCard = ({item}) => {
+  return (
+    <View style={{margin: 15}}>
+      <Pressable
+        style={{
+          backgroundColor: 'white',
+          borderRadius: 5,
+          borderTopLeftRadius: 10,
+          borderTopRightRadius: 10,
+        }}>
+        <View>
+          <Image
+            style={{
+              width: '100%',
+              height: 200,
+              borderTopLeftRadius: 10,
+              borderBottomRightRadius: 10,
+            }}
+            source={{uri: item?.image}}
+          />
+        </View>
+
+        <View style={{paddingVertical:12,paddingHorizontal:10}}>
+          <View style={{flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+            <Text>
+              {item?.name.length > 40
+                ? item?.name?.substring(0, 40) + '...'
+                : item?.name}
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                padding:6,
+                alignItems: 'center',
+                gap: 6,
+                backgroundColor: 'green',
+                borderRadius: 5,
+              }}>
+              <AntDesign name="star" size={20} color="white" />
+              <Text style={{color:'white',fontWeight:'bold'}}>{item?.rating}</Text>
+            </View>
+          </View>
+
+          <Text style={{color:'gray'}}>
+            {item?.adress?.length > 40
+              ? item?.adress?.substring(0, 40) + '...'
+              : item?.adress}
+          </Text>
+          <View style={{height:1,borderWidth:0.6,borderColor:'#E0E0E0',marginVertical:10}}>
+
+          </View>
+
+          <View style={{flexDirection:'row',alignItems:'center',justifyContent:'space-between'}}>
+            <Text>Upto 10% off</Text>
+            <Text>INR 250 Onwards</Text>
+          </View>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+export default VenueCard;
+
+const styles = StyleSheet.create({});

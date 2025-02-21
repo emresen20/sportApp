e537f04cb6f2bfd972acdda5308c47a21463764c
emresen20 <emrebernarn@gmail.com>
2025-02-21 22:48:53 +0300

    venueinfo screen

--- screens/VenueInfoScreen.js
+++ screens/VenueInfoScreen.js
@@ -10,6 +10,7 @@ import {
 import React from 'react';
 import {useRoute} from '@react-navigation/native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Amenities from './components/Amenities';
@@ -17,142 +18,182 @@ const VenueInfoScreen = () => {
   const route = useRoute();
   console.log(route?.params);
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <ScrollView>
-        <>
-          <View>
-            <Image
-              style={{width: '100%', height: 200, resizeMode: 'cover'}}
-              source={{
-                uri: 'https://images.pexels.com/photos/3660204/pexels-photo-3660204.jpeg?auto=compress&cs=tinysrgb&w=800',
-              }}
-            />
-          </View>
+    <>
+      <SafeAreaView style={{flex: 1}}>
+        <ScrollView>
+          <>
+            <View>
+              <Image
+                style={{width: '100%', height: 200, resizeMode: 'cover'}}
+                source={{
+                  uri: 'https://images.pexels.com/photos/3660204/pexels-photo-3660204.jpeg?auto=compress&cs=tinysrgb&w=800',
+                }}
+              />
+            </View>
 
-          <View style={{padding: 10}}>
-            <Text>{route?.params?.name}</Text>
+            <View style={{padding: 10}}>
+              <Text>{route?.params?.name}</Text>
+              <View
+                style={{
+                  marginTop: 5,
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  gap: 5,
+                }}>
+                <Ionicons name="time-outline" size={24} color="black" />
+                <Text style={{fontSize: 15, fontWeight: '500'}}>
+                  6:00 Am - 11:00 Pm
+                </Text>
+              </View>
+
+              <View
+                style={{
+                  marginTop: 5,
+                  flexDirection: 'row',
+                  gap: 5,
+                }}>
+                <Ionicons name="location-outline" size={24} color="black" />
+                <Text style={{fontSize: 14, width: '90%', fontWeight: '500'}}>
+                  {route?.params?.location}
+                </Text>
+              </View>
+            </View>
             <View
               style={{
-                marginTop: 5,
+                padding: 10,
                 flexDirection: 'row',
                 alignItems: 'center',
-                gap: 5,
+                justifyContent: 'space-around',
               }}>
-              <Ionicons name="time-outline" size={24} color="black" />
-              <Text style={{fontSize: 15, fontWeight: '500'}}>
-                6:00 Am - 11:00 Pm
-              </Text>
-            </View>
-
-            <View
-              style={{
-                marginTop: 5,
-                flexDirection: 'row',
-                gap: 5,
-              }}>
-              <Ionicons name="location-outline" size={24} color="black" />
-              <Text style={{fontSize: 14, width: '90%', fontWeight: '500'}}>
-                {route?.params?.location}
-              </Text>
-            </View>
-          </View>
-          <View
-            style={{
-              padding: 10,
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-around',
-            }}>
-            <View>
-              <View style={{flexDirection: 'row'}}>
-                {[0, 0, 0, 0, 0].map((en, i) => (
-                  <FontAwesome
-                    // key={`${food.id}-${i}`}
-                    style={{paddingHorizontal: 3}}
-                    name={
-                      i < Math.floor(route.params.rating) ? 'star' : 'star-half'
-                    }
-                    size={15}
-                    color="#FFD700"
-                  />
-                ))}
-                <Text>{route.params.rating} (9 ratings)</Text>
-              </View>
-              <Pressable
-                style={{
-                  marginTop: 6,
-                  width: 160,
-                  borderColor: '#686868',
-                  borderWidth: 1,
-                  borderRadius: 5,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  padding: 10,
-                }}>
-                <Text>Rate Venue</Text>
-              </Pressable>
-            </View>
-            <View style={{}}>
-              <View style={{flexDirection: 'row'}}>
-                <Text style={{textAlign: 'center'}}>100 total Activities </Text>
-              </View>
-              <Pressable
-                style={{
-                  marginTop: 6,
-                  width: 160,
-                  borderColor: '#686868',
-
-                  borderWidth: 1,
-                  borderRadius: 5,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  padding: 10,
-                }}>
-                <Text>1 Upcoming</Text>
-              </Pressable>
-            </View>
-          </View>
-          <Text style={{fontSize: 15, marginHorizontal: 10, fontWeight: '500'}}>
-            Sports Available
-          </Text>
-          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-            {route?.params?.sportsAvailable.map((item, index) => {
-              return (
-                <View
+              <View>
+                <View style={{flexDirection: 'row'}}>
+                  {[0, 0, 0, 0, 0].map((en, i) => (
+                    <FontAwesome
+                      // key={`${food.id}-${i}`}
+                      style={{paddingHorizontal: 3}}
+                      name={
+                        i < Math.floor(route.params.rating)
+                          ? 'star'
+                          : 'star-half'
+                      }
+                      size={15}
+                      color="#FFD700"
+                    />
+                  ))}
+                  <Text>{route.params.rating} (9 ratings)</Text>
+                </View>
+                <Pressable
                   style={{
+                    marginTop: 6,
+                    width: 160,
                     borderColor: '#686868',
-                    margin: 10,
-                    padding: 20,
-                    width: 130,
-                    height: 90,
                     borderWidth: 1,
                     borderRadius: 5,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: 10,
                   }}>
-                  <MaterialCommunityIcons
-                    key={index}
-                    name={item.icon}
-                    size={24}
-                    color="gray"
-                    style={{textAlign: 'center'}}
-                  />
-                  <Text
-                    style={{
-                      fontSize: 13,
-                      fontWeight: 'bold',
-                      textAlign: 'center',
-                      marginTop: 10,
-                    }}>
-                    {item?.name}
+                  <Text>Rate Venue</Text>
+                </Pressable>
+              </View>
+              <View style={{}}>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={{textAlign: 'center'}}>
+                    100 total Activities{' '}
                   </Text>
                 </View>
-              );
-            })}
-          </ScrollView>
+                <Pressable
+                  style={{
+                    marginTop: 6,
+                    width: 160,
+                    borderColor: '#686868',
 
-          <Amenities/>
-        </>
-      </ScrollView>
-    </SafeAreaView>
+                    borderWidth: 1,
+                    borderRadius: 5,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    padding: 10,
+                  }}>
+                  <Text>1 Upcoming</Text>
+                </Pressable>
+              </View>
+            </View>
+            <Text
+              style={{fontSize: 15, marginHorizontal: 10, fontWeight: '500'}}>
+              Sports Available
+            </Text>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              {route?.params?.sportsAvailable.map((item, index) => {
+                return (
+                  <View
+                    style={{
+                      borderColor: '#686868',
+                      margin: 10,
+                      padding: 20,
+                      width: 130,
+                      height: 90,
+                      borderWidth: 1,
+                      borderRadius: 5,
+                    }}>
+                    <MaterialCommunityIcons
+                      key={index}
+                      name={item.icon}
+                      size={24}
+                      color="gray"
+                      style={{textAlign: 'center'}}
+                    />
+                    <Text
+                      style={{
+                        fontSize: 13,
+                        fontWeight: 'bold',
+                        textAlign: 'center',
+                        marginTop: 10,
+                      }}>
+                      {item?.name}
+                    </Text>
+                  </View>
+                );
+              })}
+            </ScrollView>
+
+            <Amenities />
+
+            <View style={{marginHorizontal: 10}}>
+              <Text style={{fontSize: 15, fontWeight: 'bold'}}>Activities</Text>
+
+              <Pressable
+                style={{
+                  borderColor: '#787878',
+                  marginTop: 10,
+                  borderWidth: 1,
+                  justifyContent: 'center',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  gap: 10,
+                  padding: 8,
+                }}>
+                <AntDesign name="plus" size={24} color="black" />
+                <Text>Create Activity</Text>
+              </Pressable>
+            </View>
+          </>
+        </ScrollView>
+      </SafeAreaView>
+
+      <Pressable
+        style={{
+          backgroundColor: 'green',
+          padding: 10,
+          marginBottom: '5%',
+          borderRadius: 5,
+          marginHorizontal: 65,
+         
+        }}>
+        <Text style={{textAlign: 'center', color: 'white', fontWeight: 'bold'}}>
+          Book Now
+        </Text>
+      </Pressable>
+    </>
   );
 };
 

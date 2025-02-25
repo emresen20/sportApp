1e009b977dc04aca43a3bffa8be67551c753082d
emresen20 <emrebernarn@gmail.com>
2025-02-25 14:36:00 +0300

    preFinalScreen

--- screens/PreFinalScreen.js
+++ screens/PreFinalScreen.js
@@ -1,14 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Pressable, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
 const PreFinalScreen = () => {
   return (
-    <View>
-      <Text>PreFinalScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{flex: 1, backgroundColor: 'white'}}>
+      <View style={{marginTop: 80}}>
+        <Text
+          style={{
+            fontSize: 32,
+            fontWeight: 'bold',
+            fontFamily: 'GeezaPro-Bold',
+            marginLeft: 20,
+          }}>
+          All set to register
+        </Text>
+        <Text
+          style={{
+            fontSize: 32,
+            fontWeight: 'bold',
+            fontFamily: 'GeezaPro-Bold',
+            marginLeft: 20,
+            marginTop: 10,
+          }}>
+          Setting up your profile for you
+        </Text>
+      </View>
+      <Pressable
+        //onPress={registerUser}
+        style={{backgroundColor: '#03C03C', padding: 15, marginTop: 'auto'}}>
+        <Text
+          style={{
+            textAlign: 'center',
+            color: 'white',
+            fontWeight: '600',
+            fontSize: 15,
+          }}>
+          Finish Registering
+        </Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+};
 
-export default PreFinalScreen
+export default PreFinalScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});

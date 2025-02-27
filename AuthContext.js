38e5168e09d4c65264fa775ef348dd105983fbba
emresen20 <emrebernarn@gmail.com>
2025-02-27 13:36:30 +0300

    registrarinoUtils

--- AuthContext.js
+++ AuthContext.js
@@ -1,34 +1,48 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { jwtDecode } from "jwt-decode";
-import { createContext, useEffect, useState } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {jwtDecode} from 'jwt-decode';
+import {createContext, useEffect, useState} from 'react';
 
-const AuthProvider=({children})=>{
-    const [token,setToken]=useState('');
-    const [userId,setUserId]=useState('');
+const AuthContext = createContext();
 
-    const [upcominngGames,setUpComingGames]=useState([])
+const AuthProvider = ({children}) => {
+  const [token, setToken] = useState('');
+  const [userId, setUserId] = useState('');
 
-    const isLoggedIn=async ()=>{
-        try {
-            const userToken= await AsyncStorage.getItem('token')
-            setToken(userToken)
-        } catch (error) {
-            console.log('error',error)
-        }
+  const [upcominngGames, setUpComingGames] = useState([]);
+
+  const isLoggedIn = async () => {
+    try {
+      const userToken = await AsyncStorage.getItem('token');
+      setToken(userToken);
+    } catch (error) {
+      console.log('error', error);
     }
+  };
 
-    useEffect(()=>{
-        const fetchUser= async ()=>{
-            const token = await AsyncStorage.getItem("token");
-            const decodedToken= jwtDecode(token);
-            const userId=decodedToken.userId;
-            setUserId(userId)
-        }
-        fetchUser();
-    },[])
-    return(
-        <AuthContext.Provider>
-                {children}
-        </AuthContext.Provider>
-    )
-}
+  useEffect(() => {
+    const fetchUser = async () => {
+      const token = await AsyncStorage.getItem('token');
+      const decodedToken = jwtDecode(token);
+      const userId = decodedToken.userId;
+      setUserId(userId);
+    };
+    fetchUser();
+  }, []);
+
+  useEffect(() => {
+    isLoggedIn();
+  }, []);
+  return (
+    <AuthContext.Provider
+      value={{
+        token,
+        setToken,
+        userId,
+        setUserId,
+        upcominngGames,
+        setUpComingGames,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
---
+++ registrationUtlis.js
@@ -0,0 +1,24 @@
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
+// sayfalardaki kayıt işlemleri registration için asynstorageye saklayarak devam ediyor
+export const saveRegistrationPrgoress = async (screenName,data)=>{
+    try {
+        const key= `registration_progress?${screenName}`
+        await AsyncStorage.setItem(key,JSON.stringify(data))
+
+        console.log(`Progress saved for screen: ${screenName}`);
+
+    } catch (error) {
+        console.log('Error saving the progres',error)
+    }
+}
+
+export const getRegistrationProgress= async (screenName)=>{
+    try {
+        const key= `registration_progress?${screenName}`
+        const data= await AsyncStorage.getItem(key);
+        return data!== null ? JSON.parse(data) : null;
+    } catch (error) {
+        console.log('Error retreving the progress',error)
+    }
+}

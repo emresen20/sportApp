1a20d27d783585740a49368da69ef321d6526780
emresen20 <emrebernarn@gmail.com>
2025-02-28 22:14:41 +0300

    Fetch venues

--- screens/TagVenueScreen.js
+++ screens/TagVenueScreen.js
@@ -1,17 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import { useNavigation } from '@react-navigation/native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import axios from 'axios';
 
 const TagVenueScreen = () => {
-    const [venues,setVenues]=useState([]);
-    const navigation= useNavigation();
+  const [venues, setVenues] = useState([]);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const fetchVenues = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/venues')
+        setVenues(response.data)
+      } catch (error) {
+        console.log('Error fetch Venues',error)
+      }
+    }
+    fetchVenues();
+  }, []);
+  console.log('v',venues)
   return (
-    <View>
+    <SafeAreaView>
       <Text>TagVenueScreen</Text>
-    </View>
-  )
-}
+    </SafeAreaView>
+  );
+};
 
-export default TagVenueScreen
+export default TagVenueScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});

89fed157711038f1d88711ba4f9e69b1693a788a
emresen20 <emrebernarn@gmail.com>
2025-03-01 10:41:15 +0300

    refresging and loading added

--- screens/TagVenueScreen.js
+++ screens/TagVenueScreen.js
@@ -1,13 +1,15 @@
 import {
+  ActivityIndicator,
   FlatList,
   Image,
   Pressable,
+  RefreshControl,
   SafeAreaView,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import axios from 'axios';
@@ -15,34 +17,45 @@ import axios from 'axios';
 const TagVenueScreen = () => {
   const [venues, setVenues] = useState([]);
   const navigation = useNavigation();
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const fetchVenues = async () => {
+    try {
+      const response = await axios.get('http://localhost:8000/venues');
+      setVenues(response.data);
+      setLoading(false);
+      setRefreshing(false)
+    } catch (error) {
+      console.log('Error fetch Venues', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchVenues = async () => {
-      try {
-        const response = await axios.get('http://localhost:8000/venues');
-        setVenues(response.data);
-      } catch (error) {
-        console.log('Error fetch Venues', error);
-      }
-    };
+    setLoading(true);
     fetchVenues();
   }, []);
   console.log('v', venues);
 
-  const [taggedVenue,setTaggedVenue]=useState(null)
+  const [taggedVenue, setTaggedVenue] = useState(null);
 
-  useEffect(()=>{
-    if(taggedVenue){
-        navigation.goBack({taggedVenue})
+  useEffect(() => {
+    if (taggedVenue) {
+      navigation.goBack({taggedVenue});
     }
-  },[taggedVenue,navigation])
+  }, [taggedVenue, navigation]);
 
+  handleSelectVenue = venue => {
+    navigation.navigate('Create', {taggedVenue: venue});
+  };
 
-  handleSelectVenue=(venue)=>{
-    navigation.navigate('Create',{taggedVenue:venue})
-  }
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    fetchVenues(); // Yeniden verileri çek
+  }, []);
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{flex: 1}}>
       <View
         style={{
           padding: 10,
@@ -64,67 +77,75 @@ const TagVenueScreen = () => {
           </Text>
         </View>
       </View>
-      <FlatList
-        data={venues}
-        keyExtractor={item => item._id}
-        renderItem={({item}) => (
-          <Pressable
-            onPress={() => handleSelectVenue(item?.name)}
-            style={{
-              padding: 10,
-              marginVertical: 10,
-              borderColor: '#e0e0e0',
-              borderWidth: 1,
-              marginHorizontal: 10,
-            }}>
-            <View>
-              <View style={{flexDirection: 'row', gap: 10}}>
-                <Image
-                  style={{
-                    width: 90,
-                    height: 90,
-                    resizeMode: 'cover',
-                    borderRadius: 7,
-                  }}
-                  source={{
-                    uri: item?.image,
-                  }}
-                />
+      {loading ? (
+        <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+          <ActivityIndicator size={'large'} color={'#294461'} />
+        </View>
+      ) : (
+        <FlatList
+          data={venues}
+          keyExtractor={item => item._id}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+          renderItem={({item}) => (
+            <Pressable
+              onPress={() => handleSelectVenue(item?.name)}
+              style={{
+                padding: 10,
+                marginVertical: 10,
+                borderColor: '#e0e0e0',
+                borderWidth: 1,
+                marginHorizontal: 10,
+              }}>
+              <View>
+                <View style={{flexDirection: 'row', gap: 10}}>
+                  <Image
+                    style={{
+                      width: 90,
+                      height: 90,
+                      resizeMode: 'cover',
+                      borderRadius: 7,
+                    }}
+                    source={{
+                      uri: item?.image,
+                    }}
+                  />
 
-                <View style={{flex: 1}}>
-                  <Text
-                    numberOfLines={1}
-                    ellipsizeMode="tail"
-                    style={{fontSize: 15, fontWeight: '500', width: '100%'}}
-                    >
-                    {item?.name}
-                  </Text>
+                  <View style={{flex: 1}}>
+                    <Text
+                      numberOfLines={1}
+                      ellipsizeMode="tail"
+                      style={{fontSize: 15, fontWeight: '500', width: '100%'}}>
+                      {item?.name}
+                    </Text>
 
-                  <Text style={{marginTop: 5, color: 'gray'}}>
-                    {item?.address}
-                  </Text>
+                    <Text style={{marginTop: 5, color: 'gray'}}>
+                      {item?.address}
+                    </Text>
 
-                  <Text style={{marginTop: 8, fontWeight: '500'}}>
-                    4.4 (122 ratings)
-                  </Text>
+                    <Text style={{marginTop: 8, fontWeight: '500'}}>
+                      4.4 (122 ratings)
+                    </Text>
+                  </View>
+
+                  <Ionicons
+                    name="shield-checkmark-sharp"
+                    size={24}
+                    color="green"
+                  />
                 </View>
 
-                <Ionicons
-                  name="shield-checkmark-sharp"
-                  size={24}
-                  color="green"
-                />
+                <View>
+                  <Text style={{textAlign: 'center', color: 'gray'}}>
+                    BOOKABLE
+                  </Text>
+                </View>
               </View>
-
-              <View>
-                <Text style={{textAlign: 'center', color: 'gray'}}>
-                  BOOKABLE
-                </Text>
-              </View>
-            </View>
-          </Pressable>
-        )}
-      />
+            </Pressable>
+          )}
+        />
+      )}
     </SafeAreaView>
   );
 };

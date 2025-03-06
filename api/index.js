8ede9f267c416c1c7f7511f4fa31eae6ead840c2
emresen20 <emrebernarn@gmail.com>
2025-03-06 14:27:08 +0300

    user endpoint

--- api/index.js
+++ api/index.js
@@ -833,3 +833,19 @@ app.get('/games/:gameId/requests',async(req,res)=>{
     res.status(500).json({message: 'Failed to get req'});
   }
 })
+
+app.get('/user/:userId', async (req,res)=>{
+  try {
+    const {userId}=req.params;
+    const user= await User.findById(userId);
+
+    if(!user){
+      return res.status(200).json({message:'User not found'})
+    }
+
+    return res.status(200).json({user})
+  } catch (error) {
+    console.error(err);
+    res.status(500).json({message: 'Failed to fetch user'});
+  }
+})

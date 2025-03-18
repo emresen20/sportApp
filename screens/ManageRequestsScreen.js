4621dda7a9bede1dcc7f868cbbfb7b85e50482bb
emresen20 <emrebernarn@gmail.com>
2025-03-18 21:39:34 +0300

    MageReq

---
+++ screens/ManageRequestsScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ManageRequestsScreen = () => {
+  return (
+    <View>
+      <Text>ManageRequestsScreen</Text>
+    </View>
+  )
+}
+
+export default ManageRequestsScreen
+
+const styles = StyleSheet.create({})
